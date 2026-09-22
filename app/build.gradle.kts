2e2536c5d1a91fcb405b6e15a4f967469df254b7
CHEYER CORPUS GIRALDO <cheyergc@gmail.com>
2026-09-22 02:05:29 -0500

    release: prepare CHETO 1.0.0-rc2 after R8 fix

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,8 +28,8 @@ android {
         applicationId = "com.cmcorpusg.chetoauthenticator"
         minSdk = 26
         targetSdk = 36
-        versionCode = 16
-        versionName = "1.0.0-rc1"
+        versionCode = 17
+        versionName = "1.0.0-rc2"
 
         buildConfigField("String", "GOOGLE_WEB_CLIENT_ID", googleWebClientId.asBuildConfigString())
         buildConfigField("String", "MICROSOFT_CLIENT_ID", microsoftClientId.asBuildConfigString())
