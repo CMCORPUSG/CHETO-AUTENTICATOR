1a3bde8556a8c41891f2b7f8eac901b00a24f6e8
CHEYER CORPUS GIRALDO <cheyergc@gmail.com>
2026-09-21 23:26:07 -0500

    auth: fix Microsoft client id typo

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,7 +10,7 @@ fun String.asBuildConfigString(): String =
 
 val googleWebClientId = providers.gradleProperty("CHETO_GOOGLE_WEB_CLIENT_ID").orElse("").get()
 val microsoftClientId = providers.gradleProperty("CHETO_MICROSOFT_CLIENT_ID")
-    .orElse("78454b61-cee1-423c-a27b-73157f6698de")
+    .orElse("78454d61-cee1-423c-a27b-73157f6698de")
     .get()
 
 val releaseKeystoreFile = rootProject.file("keystore.properties")
